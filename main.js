9e075989adc2fe1362c12a30d41439066d8259d9
LisaDash10 <81564491+LisaDash10@users.noreply.github.com>
2022-04-19 10:30:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 song_1 = "";
 song_2 = "";
 song_playing = "";
+song_2_playing = "";
 
 leftWristX = 0;
 rightWristX = 0;
 leftWristY = 0;
 rightWristY = 0;
 score_leftWrist = 0;
+score_rightWrist = 0;
 
 function preload() {
     song_1 = loadSound("music.mp3");
@@ -30,6 +32,18 @@ function draw() {
     fill('#FF0000');
     stroke('#FF0000');
     song_playing = song_1.isPlaying();
+    song_2_playing = song_2.isPlaying();
+
+    if(score_rightWrist > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+        song_1.stop();
+
+        if(song_2_playing = false) {
+            song_2.play();
+            document.getElementById("song_name").innerHTML = "Song Name - Peter Pan ";
+        }
+    }
+
     if(score_leftWrist > 0.2) {
         circle(leftWristX, leftWristY, 20);
         song_2.stop();
@@ -50,8 +64,9 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+        score_rightWrist = results[0].pose.keypoints[10].score;
         score_leftWrist = results[0].pose.keypoints[9].score;
-        console.log("score of leftWrist = " + score_leftWrist);
+        console.log("score of leftWrist = " + score_leftWrist + " score of rightWrist = " + score_rightWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWrist X = " + leftWristX + "leftWrist Y = " + leftWristY);
